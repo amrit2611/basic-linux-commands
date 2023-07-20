db2cce61a6167cfbdaf9e17ebbde66343026fa02
Amrit Dhandharia <81375472+amrit2611@users.noreply.github.com>
2023-07-20 20:34:50 +0530

    Update and rename other-commands to other-commands.sh

---
+++ other-commands.sh
@@ -0,0 +1,7 @@
+which bash
+#this command outputs the path of the bash we are currently using
+
+
+chmod 777 <filename>
+#'chmod' command used to modify permissions
+#here '777' would provide read, write and executable permissions for the specified file
