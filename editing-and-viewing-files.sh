8a4807d855446e5b284ce781fb928a4248fa4d4b
Amrit Dhandharia <81375472+amrit2611@users.noreply.github.com>
2023-07-20 20:30:35 +0530

    Update and rename editing-and-viewing-files to editing-and-viewing-files.sh

---
+++ editing-and-viewing-files.sh
@@ -0,0 +1,21 @@
+nano filename
+#opens file in nano editor
+
+cat file1.txt
+#See the content of the file
+
+more file1.txt
+#we can scroll down using space (pagewise) or enter (line wise)
+#for large files. shows content page-wise
+
+less file1.txt
+#we can scroll up or down the file using arrows
+
+q
+#to exit from more or less
+
+head -n 5 file1.txt
+#shows the first 5 lines of the file 
+
+tail -n 5 file1.txt
+#shows the last 5 lines of the file
